561595c31abacbf8ba0e58a1fbcfd7c279b6a989
0xOneiros <154099755+0xOneiros@users.noreply.github.com>
2024-06-24 05:16:20 -0800

    Add files via upload

---
+++ src/displayHeader.js
@@ -0,0 +1,13 @@
+const colors = require('colors');
+
+function displayHeader() {
+  process.stdout.write('\x1Bc');
+  console.log(colors.cyan('========================================'));
+  console.log(colors.cyan('=      Units Network Testnet BOT       ='));
+  console.log(colors.cyan('=                                      ='));
+  console.log(colors.cyan('=    https://x.com/0xoneiros           ='));
+  console.log(colors.cyan('========================================'));
+  console.log();
+}
+
+module.exports = displayHeader;
